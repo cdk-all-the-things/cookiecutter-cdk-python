9b7e259cfa879a1af416f5da523f9a594909f1ba
Randy Ridgley <rridgley@amazon.com>
2024-01-03 12:38:12 -0500

    feat(initial commit)

--- {{cookiecutter.service_name}}/scripts/bootstrap.sh
+++ {{cookiecutter.service_name}}/scripts/bootstrap.sh
@@ -33,22 +33,6 @@ echo "⭐ Setup git "
 git init
 git remote add origin "http://www.github.com/{{cookiecutter.git_repo_url}}"
 
-echo "⭐ Installing pre-commit hook "
-echo "NOTE: This requires sudo to run 'git defender --setup'"
-# Needs to use git_defender to run this to fix incompatibility with core.hooksPath
-# https://github.com/pre-commit/pre-commit/issues/1198
-sudo git defender -v --precommit_tool_setup
-
-# Need to run this to install commit-msg type hooks (not installed via git defender)
-sudo git config --system --unset-all core.hookspath
-pre-commit install --hook-type commit-msg
-sudo git defender --install
-pre-commit validate-config
-
-echo "⭐ Run git defender"
-echo "---------------------------------------------"
-git defender --setup
-echo "---------------------------------------------"
 echo "⭐ Initial Commit using Commitizen"
 git add -A
 git commit -m "build: initial commit"
